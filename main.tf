539ac1d5e5ba07d7a04846cd86962fef84dc706f
Ricardo Amador <32242716+ricardoamador@users.noreply.github.com>
2024-09-12 17:14:50 -0700

    remove deprecated field

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "blog_vpc" {
 }
 
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source   = "terraform-aws-modules/autoscaling/aws"
   version  = "8.0.0"
   name     = "blog"
@@ -43,8 +43,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups      = [module.blog_sg.security_group_id]
+  security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
