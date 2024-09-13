507be186cb0eed9ad5a1cd8587b86d29e7ab1249
Ricardo Amador <32242716+ricardoamador@users.noreply.github.com>
2024-09-12 18:54:44 -0700

    Fix syntax error with type variables.

--- variables.tf
+++ variables.tf
@@ -6,13 +6,13 @@ variable "instance_type" {
 variable "ami_filter" {
   description = "Name filter and owner for AMI"
   
-  type object({
-    name = string
+  type    = object({
+    name  = string
     owner = string
   })
   
   default = {
-    name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
     owner = "979382823631"
   }
 }
@@ -20,7 +20,7 @@ variable "ami_filter" {
 variable "environment" {
   description = "Development environment"
   
-  type object({
+  type = object({
     name           = string
     network_prefix = string
   })
@@ -34,10 +34,10 @@ variable "environment" {
 # For autoscaling groups
 variable "asg_min_size" {
   description = "Minimum number of instances in the ASG (autoscaling group)"
-  default = 1
+  default     = 1
 }
 
 variable "asg_max_size" {
   description = "Maximum number of instances in the ASG"
-  default = 2
+  default     = 2
 }
