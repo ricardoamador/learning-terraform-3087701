777c1a572a0d5687892473c199972969da5e6d27
Ricardo Amador <32242716+ricardoamador@users.noreply.github.com>
2024-09-14 12:08:59 -0700

    Define QA Environment

---
+++ qa/main.tf
@@ -0,0 +1,12 @@
+# need this file to specify the module
+module "qa" {
+    source = "../modules/blog"
+
+    environment = {
+        name           = "qa"
+        network_prefix = "10.1"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1
+}
